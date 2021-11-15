574e6067d9f7f8d29f1f0114a56bda82305e0b87
Siegfried Dubois <dubois.siegfried@gmail.com>
2021-11-15 12:01:49 +0100

    Noyau du générateur de noms

--- code/main.py
+++ code/main.py
@@ -8,20 +8,33 @@ def main():
 
 class Tools:
     def loi_normale(mu, sigma):
-        n = np.random.randn(1) * sigma + mu
+        "Renvoie le premier entier strictement positif généré selon une loi normale."
+        n = 0
+        while (n<=0):
+            n = np.random.randn(1) * sigma + mu
         return int(n[0])
+
+    def gen_nom(sexe,ld):
+        "Sexe doit être au format M ou F"
+        opener,closer,mid1,mid2,a = random.choice(ld.dict["Opener"]),random.choice(ld.dict["Closer"+sexe]),random.choice(ld.dict["Middle"]),random.choice(ld.dict["Middle"]),random.randrange(3)
+        if(a==0): return opener+mid1+mid2+closer
+        elif(a==1): return opener+closer+mid1+closer
+        else: return opener+closer
 
 class Pnj:
     def __init__(self,ld):
-        self.name = random.choice(ld.dict["Nom"])
-        self.age = Tools.loi_normale(25,15)
+        "Initialise un nouvel objet PnJ"
+        self.sexe = "F" if(random.randrange(1)==0) else "M"
+        self.name = Tools.gen_nom(self.sexe,ld)
+        self.age = Tools.loi_normale(30,20)
 
     def __str__(self):
+        "Renvoie une description du PnJ"
         return f"{self.name} est âgé de {self.age} ans."
 
 class Loader:
     def __init__(self,fichier):
-        "Dico de listes"
+        "Chargement du fichier de données en un dico de listes"
         self.dict = {}
         with open(fichier,"r") as reader:
             for l in reader:
