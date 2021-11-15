8f337e6aff20af0260b722bedf2a6e59e7ecc28d
Siegfried Dubois <dubois.siegfried@gmail.com>
2021-11-15 11:41:58 +0100

    Ajout du parser de fichier de données

--- code/main.py
+++ code/main.py
@@ -1,7 +1,9 @@
+import random
 import numpy as np
 
 def main():
-    monPnj = Pnj("Ulfric Sombrage")
+    ld = Loader("/home/siegfried/Python/PNJMaker/ressources/data.ini")
+    monPnj = Pnj(ld)
     print(monPnj)
 
 class Tools:
@@ -10,12 +12,23 @@ class Tools:
         return int(n[0])
 
 class Pnj:
-    def __init__(self,name):
-        self.name = name
+    def __init__(self,ld):
+        self.name = random.choice(ld.dict["Nom"])
         self.age = Tools.loi_normale(25,15)
 
     def __str__(self):
         return f"{self.name} est âgé de {self.age} ans."
+
+class Loader:
+    def __init__(self,fichier):
+        "Dico de listes"
+        self.dict = {}
+        with open(fichier,"r") as reader:
+            for l in reader:
+                l = l[:-1]
+                a,b = l.split(":")[0],l.split(":")[1]
+                self.dict[a] = b.split(",")
+
 
 
 # si on lance via ligne de commande, on exécute la fonction main
