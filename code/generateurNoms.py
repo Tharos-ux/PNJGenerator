7f30600d42c0f2a914c55dc28b520de5a18e5658
Siegfried Dubois <dubois.siegfried@gmail.com>
2022-01-17 15:52:32 +0100

    datas

--- code/generateurNoms.py
+++ code/generateurNoms.py
@@ -9,14 +9,14 @@ def sauvegarde(texteA,texteB):
 
 def core():
     listM,listF = [],[]
-    ld = main.Loader("data.ini")
+    ld = main.Loader("data/data.ini")
     for _ in range(1000):
         nomM,nomF = main.Tools.gen_nom("Masculin",ld,neuralInput=True),main.Tools.gen_nom("Féminin",ld,neuralInput=True) # tuples
         listMt=[str(x) for x in nomM[0]]
         listFt=[str(x) for x in nomF[0]]
         listMt,listFt = ','.join(listMt),','.join(listFt)
-        listF.append(f"{listFt},{nomF[1]}")
-        listM.append(f"{listMt},{nomM[1]}")
+        listF.append(f"{nomF[1]},,{listFt}")
+        listM.append(f"{nomM[1]},,{listMt}")
     sauvegarde(listM,listF)
 
 core()
