e7e8330e6a2f4589b949d8d04ff3ada0c973ba1c
Siegfried Dubois <dubois.siegfried@gmail.com>
2022-02-14 17:46:44 +0100

    pnj refactoring

--- code/reg/pnj.py
+++ code/reg/pnj.py
@@ -15,9 +15,11 @@ class Pnj:
                 self.carac,self.constraints = t.Tools.fill_regexpr(self.dico,{'Nom' : self.name,'Age' : self.age, 'Sexe_biologique' : self.sexe})
                 self.desc = ""
             case _:
+                self.dico = ld
                 self.sexe = current.sexe
                 self.name = current.name
                 self.age = current.age
+                self.constraints = None
                 match change:
                     case 'Sexe_biologique':
                         # on intervertit l'état
@@ -28,10 +30,13 @@ class Pnj:
                     case 'Age':
                         self.age = current.carac['Age'] = t.Tools.loi_normale(30,8)
                     case car:
-                        # TODO chaining des expressions par arbres
+
                         list_reroll = []
+
                         for char,filtre in current.constraints.items():
                             if (car in filtre): list_reroll.append(char)
+
+                        # liste noire : 
                         blacklist = [key for key in current.carac if key != car]
                         nextchar = current.carac[car]
                         while nextchar==current.carac[car]:
@@ -43,16 +48,28 @@ class Pnj:
                         blacklist = [key for key in current.carac if key not in list_reroll]
                         newchars,self.constraints = t.Tools.fill_regexpr(self.dico,{'Nom' : self.name,'Age' : self.age, 'Sexe_biologique' : self.sexe, change : current.carac[change]},blacklist)
                         charlist = t.Tools.fill_regexpr(self.dico,{'Nom' : self.name,'Age' : self.age, 'Sexe_biologique' : self.sexe, change : current.carac[change]},["Opener","CloserM","CloserF","Middle"])[0].keys()
-                        print(charlist)
-                        for e in list_reroll:
-                            current.carac[e] = newchars[e]
+                        print(newchars)
 
+                        
+                        for e in list_reroll:
+                            print(f"Clé {e} : ancienne char {current.carac[e]} nouvelle char {newchars[e]}")
+                            if e in current.carac:
+                                current.carac[e] = newchars[e]
+                            else:
+                                print("Le cas inconnu vient de se produire")
+                        
                         newdict = dict()
                         for key in charlist:
-                            newdict[key] = newchars[key] if key in newchars.keys() else current.carac[key]
+                            if key in newchars.keys():
+                                newdict[key] = newchars[key]
+                            elif key in current.carac.keys():
+                                newdict[key] = current.carac[key]
+                            else:
+                                pass
                         current.carac = newdict
                         
-                self.carac = current.carac
+                        
+                self.carac = newchars
                 self.desc = current.desc
 
     def __str__(self):
